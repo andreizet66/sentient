d8293cf9ba57d9f0f8268d429dcf3bf6a6f1c85b
Andrei Zet <65792896+andreizet66@users.noreply.github.com>
2025-11-07 21:06:21 +0200

    Change Uvicorn port from 8000 to 8332

--- start.py
+++ start.py
@@ -4,6 +4,6 @@ import sys
 print("Starting Sentient WebUI ...\n")
 
 try:
-    subprocess.check_call([sys.executable, "-m", "uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000"])
+    subprocess.check_call([sys.executable, "-m", "uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8332"])
 except KeyboardInterrupt:
     print("\nStopped.")
